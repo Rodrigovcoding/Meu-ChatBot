82dc053b278f09cda103d59b9c28b0bf7c0a4e20
Rodrigovcoding <142947246+Rodrigovcoding@users.noreply.github.com>
2025-04-01 19:20:18 -0300

    teste

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function sendMessage() {
 
     // Faz a requisição ao backend
     try {
-        const response = await fetch ("https://meu-chatbot.onrender.com/chat", {
+        const response = await fetch ("https://meu-chatbot-3.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ prompt: userInput })
