6ff8de682039e2b7d53e58e305eaa390c49f20b9
Rodrigovcoding <142947246+Rodrigovcoding@users.noreply.github.com>
2025-04-01 20:41:46 -0300

    teste

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import google.generativeai as gg
-from dotenv import load_dotenv
+import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 GOOGLE_GEMINI_API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
-gg.configure(api_key=GOOGLE_GEMINI_API_KEY)
+genai.configure(api_key=GOOGLE_GEMINI_API_KEY)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rodrigovcoding.github.io"],
+    allow_origins=[
+        "https://rodrigovcoding.github.io",  # Permite GH Pages
+        "http://127.0.0.1:5500"              # Se quiser testar local
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,6 +27,6 @@ class PromptRequest(BaseModel):
 
 @app.post("/chat")
 async def chat(request: PromptRequest):
-    model = gg.GenerativeModel("gemini-1.5-pro-latest")
+    model = genai.GenerativeModel("models/gemini-1.0-pro-vision-latest")
     response = model.generate_content(request.prompt)
     return {"response": response.text}
