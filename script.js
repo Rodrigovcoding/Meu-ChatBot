1dc5db12d03f765fad24b7e2fc9dd8fe499272be
offdev <vieirar367@gmail.com>
2025-04-01 17:57:41 -0300

    commit arduino

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function sendMessage() {
 
     // Faz a requisição ao backend
     try {
-        const response = await fetch("http://127.0.0.1:8000/chat", {
+        const response = await fetch("https://meu-chatbot.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ prompt: userInput })
